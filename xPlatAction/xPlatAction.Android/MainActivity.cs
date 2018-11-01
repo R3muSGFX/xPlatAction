7b8d14e3036f57802341e56fee78aad94e246181
Remus - Daniel Moisi <remus.moisi@outlook.com>
2018-11-01 15:39:19 +0200

    added backend project

--- xPlatAction/xPlatAction.Android/MainActivity.cs
+++ xPlatAction/xPlatAction.Android/MainActivity.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace xPlatAction.Droid
 {
-    [Activity(Label = "xPlatAction", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "xPlatAction", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,6 +13,8 @@ namespace xPlatAction.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
--- xPlatAction/xPlatAction/Views/MainPage.xaml.cs
+++ xPlatAction/xPlatAction/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System.Linq;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace xPlatAction.Views
@@ -10,5 +12,22 @@ namespace xPlatAction.Views
 		{
 			InitializeComponent();
 		}
+
+		private async void BtnClick_Clicked(object sender, System.EventArgs e)
+		{
+			MobileServiceClient client = new MobileServiceClient("http://192.168.35.206:5282");
+			var items = await client.GetTable<TodoItem>().ReadAsync();
+			var item = items.FirstOrDefault();
+			lblMsg.Text = item.Text;
+		}
+	}
+
+	public class TodoItem
+	{
+		public string Id { get; set; }
+
+		public string Text { get; set; }
+
+		public bool Complete { get; set; }
 	}
 }
